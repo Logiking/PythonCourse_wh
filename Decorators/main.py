a616b92fb59b276da1abe8dd4064aba2728d0ba1
wsm <2481583570@qq.com>
2023-05-17 21:49:16 +0800

    first commit

---
+++ Decorators/main.py
@@ -0,0 +1,35 @@
+def foo():
+    print("foo")
+
+def bar(func):
+    print("1")
+    func()
+
+# traditional use
+def logging(func):
+    def wrapper():
+        print('hh')
+        return func()
+    return wrapper
+
+foo = logging(foo)
+foo()
+
+# normal use
+@logging
+def foo():
+    print('foov1')
+
+foo()
+
+def logging(func):
+    def wrapper(*args, **kwargs):
+        print('update')
+        return func(*args, **kwargs)
+    return wrapper
+
+@logging
+def foo(name):
+    print(name)
+
+foo()
